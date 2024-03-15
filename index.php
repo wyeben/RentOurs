0d72f1ecd16a1f7705399c4da09ad60b453acf41
Wyeben <yila.ben@yahoo.com>
2024-03-15 07:53:24 +0100

    added code

--- index.php
+++ index.php
@@ -239,7 +239,6 @@ function postCar($make, $model, $year, $type, $availability, $paymentAmount) {
     $stmt->bindParam(':model', $model);
     $stmt->bindParam(':year', $year);
     $stmt->bindParam(':type', $type);
-
     $stmt->bindParam(':availability', $availability);
     $stmt->bindParam(':paymentAmount', $paymentAmount);
     $stmt->execute();
